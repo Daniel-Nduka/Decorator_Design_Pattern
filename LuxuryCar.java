0b71c43d6b184191dc58f62065fb1a424b8af787
Nduka Ofoeyeno <ndukaofoeyeno@MacBook-Pro-5.local>
2024-02-19 15:40:10 +0000

    second commit

---
+++ LuxuryCar.java
@@ -0,0 +1,12 @@
+//concrete component class
+package Lab3_2;
+
+public class LuxuryCar extends Car{
+	
+	public Double getPrice() {
+		return 50000.0;
+	}
+	public String getDescription() {
+		return "is a shinier, faster car than the basic one";
+	}
+}
